5a1e443eb83a4f5ecef1641a1c0b435fa3048d4c
Satoshishi00 <webmaster-2.0@hotmail.com>
2020-02-26 12:37:29 +0100

    Ajout de chaque agent à une zone

--- model.py
+++ model.py
@@ -23,12 +23,6 @@ class Position:
     def latitude(self):
         return self.latitude_degrees * math.pi / 180
 
-# --- Zone
-# Position coin inf gauche
-# Position coin inf droit
-# Habitants
-# Par defaut la zone est vide
-
 
 class Zone:
 
@@ -43,7 +37,7 @@ class Zone:
     def __init__(self, corner1, corner2):
         self.corner1 = corner1
         self.corner2 = corner2
-        self.inhabitants = 0
+        self.inhabitants = []
 
     @classmethod
     def initialize_zones(cls):
@@ -56,15 +50,56 @@ class Zone:
                 cls.ZONES.append(zone)
         print(len(cls.ZONES))
 
+    # Verifie qu'une position appartient bien à une zone
+    def contains(self, position):
+        return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+            position.longitude < max(self.corner1.longitude, self.corner2.longitude) and \
+            position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+            position.latitude < max(
+                self.corner1.latitude, self.corner2.latitude)
+
+    @classmethod
+    # Retourne la zone à laquelle appartient une position
+    def find_zone_that_contains(cls, position):
+        # Détermination de l'index de la zone à laquelle appartient une position
+        longitude_index = int(
+            (position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        latitude_index = int(
+            (position.latitude_degrees - cls.MIN_LATITUDE_DEGREES) / cls.HEIGHT_DEGREES)
+        longitude_bins = int(
+            (cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        zone_index = latitude_index * longitude_bins + longitude_index
+
+        zone = cls.ZONES[zone_index]
+        # Vérification de que la position puisse bien appartenir à cette zone
+        assert zone.contains(position)
+
+        return zone
+
+    def add_inhabitant(self, inhabitant):
+        self.inhabitants.append(inhabitant)
+
+    @property
+    def population(self):
+        return len(self.inhabitants)
+
 
 def main():
+    # Création du plateau et des zones
+    Zone.initialize_zones()
+
     for agent_attributes in json.load(open("agents-100k.json")):
+        # Détermination de la position de l'agent
         longitude = agent_attributes.pop("longitude")
         latitude = agent_attributes.pop("latitude")
-        position = Position(latitude, longitude)
+        position = Position(longitude, latitude)
+        # Création de l'agent
         agent = Agent(position, **agent_attributes)
-        print(agent.position.longitude)
-    Zone.initialize_zones()
+        # Enregistrement de l'agent dans la zone correspondant à sa position
+        zone = Zone.find_zone_that_contains(position)
+        zone.add_inhabitant(agent)
+        # Affichage de la population de la zone à laquelle appartient cet agent
+        print(zone.population)
 
 
 main()
